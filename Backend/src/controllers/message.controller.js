0ab767cccd2247049818191be5b3411e201350ff
Abdullah <95abdullah99@gmail.com>
2024-11-24 18:59:58 +0530

    send msges and impage upload api

--- Backend/src/controllers/message.controller.js
+++ Backend/src/controllers/message.controller.js
@@ -12,9 +12,9 @@ try {
     res.status(200).json(filteredUsers)
 } catch (error) {
     console.error("Error in getUserForSidebar" , error.message);
-    res.status(500).json({error : "Internal server error"});
+    return res.status(500).json({error : "Internal server error"});
 }
-}
+};
 
 //get messeges to show 
 export const getMessages = async (req , res) => {
@@ -36,6 +36,37 @@ export const getMessages = async (req , res) => {
         res.status(200).json(messages)
     } catch (error) {
         console.log("Error in getMessages controller : " , error.message);
-        res.status(500).json({error : "Internal server error"});
+        return res.status(500).json({error : "Internal server error"});
     }
-}
+};
+
+//Send message api
+export const sendMessage = async (req, res) => {
+    try {
+        const {text , image} = req.body;
+        const {id : receiverId} = req.params;
+        const senderId = req.user._id;
+
+        // checking if user sending image or not
+        let imageUrl;
+
+        if (image) {
+            //upload image to cloudinary
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image : imageUrl,
+        })
+
+        await newMessage.save();
+        res.status(201).json(newMessage)
+    } catch (error) {
+        console.log("Error in sendMessage controller : " , error.message);
+        return res.status(500).json({error : "Internal server error"});
+    }
+};
