3e32bf38c931de1e5454d6ab2e5b456b998a669a
Abdullah <95abdullah99@gmail.com>
2024-11-24 18:48:07 +0530

    fetch messages form send and reciver

--- Backend/src/controllers/message.controller.js
+++ Backend/src/controllers/message.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model";
+import Message from "../models/message.model.js"
 
+//Get user for sidebar
 export const getUserForSidebar = async (req , res) => {
 try {
     //grabbign logged in user id  form database
@@ -12,4 +14,28 @@ try {
     console.error("Error in getUserForSidebar" , error.message);
     res.status(500).json({error : "Internal server error"});
 }
+}
+
+//get messeges to show 
+export const getMessages = async (req , res) => {
+    try {
+        const {id : userToChatId } = req.params 
+        const senderId = req.user._id;
+
+        //finding the messages of both sender or revciver 
+        const messages = await Message.find({
+            $or : [
+                {
+                    senderId : senderId , receiverId : userToChatId
+                },
+                {
+                    senderId : userToChatId , receiverId : senderId
+                }
+            ]
+        })
+        res.status(200).json(messages)
+    } catch (error) {
+        console.log("Error in getMessages controller : " , error.message);
+        res.status(500).json({error : "Internal server error"});
+    }
 }
