9a215a0d0d46af5c60608e6c000da8e6fccf4cc2
Abdullah <95abdullah99@gmail.com>
2024-11-26 20:36:04 +0530

    Routes setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './Components/Navbar';
+import {Routes , Route} from "react-router-dom";
+import Home from './Pages/Home';
+import Signup from './Pages/Signup';
+import Login from './Pages/Login';
+import Settings from './Pages/Settings';
+import Profilepage from './Pages/Profilepage';
 
 const App = () => {
   return (
     <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/settings' element={<Settings/>}/>
+        <Route path='/profile' element={<Profilepage/>}/>
+      </Routes>
     </div>
   )
 }
