1989bdcff5d52e75dbd09054927341c6e76958e8
Abdullah <95abdullah99@gmail.com>
2024-11-28 18:10:49 +0530

    settng up axios and cors and rounte protection

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from './Components/Navbar';
-import {Routes , Route} from "react-router-dom";
+import {Routes , Route, Navigate} from "react-router-dom";
 import {useAuthStore} from "./store/useAuthStore.js"
 import Home from './Pages/Home';
 import Signup from './Pages/Signup';
 import Login from './Pages/Login';
 import Settings from './Pages/Settings';
 import Profilepage from './Pages/Profilepage';
+import {Loader} from "lucide-react";
 
 const App = () => {
-const {authUser} = useAuthStore();
+  //Destructure funtions here
+const {authUser , checkAuth , isCheckingAuth} = useAuthStore();
+  //Calling checkAuth here
+  useEffect(() => {
+    checkAuth()
+  } , [checkAuth])
+  console.log({authUser})
+
+  if (isCheckingAuth && !authUser) return (
+    <div className="flex items-center justify-center h-screen">
+      <Loader className="size-10 animate-spin" />
+    </div>
+  )
   return (
     <div>
       <Navbar/>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/signup' element={<Signup/>}/>
-        <Route path='/login' element={<Login/>}/>
+        <Route path='/' element={authUser ? <Home/> : <Navigate to = "/login" />}/>
+        <Route path='/signup' element={!authUser ? <Signup/> : <Navigate to = "/" />}/>
+        <Route path='/login' element={!authUser ? <Login/> : <Navigate to = "/" />}/>
         <Route path='/settings' element={<Settings/>}/>
-        <Route path='/profile' element={<Profilepage/>}/>
+        <Route path='/profile' element={authUser ? <Profilepage/> :  <Navigate to = "/login" />}/>
       </Routes>
     </div>
   )
--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -1,6 +1,6 @@
 //Using zustand for global state management
 import {create} from "zustand";
-
+import {axiosInstance} from "../Lib/axios.js"
 //passed object is out initial state 
 //We can use set to update the state which is a inbuilt function
 export const useAuthStore = create((set) => ({
@@ -10,4 +10,18 @@ export const useAuthStore = create((set) => ({
     isUpdatingProfile : false,
     //initially its null bcoz we'll check it later
     isCheckingAuth : true,
+
+    checkAuth : async () => {
+        try {
+            const res = await axiosInstance.get("/auth/check");
+            //Set authenticaation data if user authenticated with correct credential
+            set({authUser : res.data})
+        } catch (error) {
+            //null if its wrong
+            console.log("error in checkauth" , error)
+            set({authUser : null})
+        } finally {
+            set({ isCheckingAuth : false});
+        }
+    }
 }));
